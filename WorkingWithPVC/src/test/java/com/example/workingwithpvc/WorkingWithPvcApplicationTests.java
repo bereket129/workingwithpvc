70f7092a9eb64e5a57eace228b51cff0b83172e5
bereket129 <abusha129@gmail.com>
2022-03-21 16:50:23 -0500

    Add files via upload

---
+++ WorkingWithPVC/src/test/java/com/example/workingwithpvc/WorkingWithPvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workingwithpvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkingWithPvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
