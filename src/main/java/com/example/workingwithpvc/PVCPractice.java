440c5fb1f3f7274bf71819d947e498dae4921bd9
bereket129 <abusha129@gmail.com>
2022-03-23 12:42:36 -0500

    Update PVCPractice.java

--- src/main/java/com/example/workingwithpvc/PVCPractice.java
+++ src/main/java/com/example/workingwithpvc/PVCPractice.java
@@ -1,14 +1,14 @@
 package com.example.workingwithpvc;
-
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Properties;
+import java.util.logging.Logger;
 
 @RestController
 public class PVCPractice {
+    public final Logger LOGGER = Logger.getLogger(this.getClass().getName());
 
     @GetMapping("/write")
     public Boolean hello(@RequestParam String key, @RequestParam String value) throws IOException {
@@ -23,11 +23,13 @@ public class PVCPractice {
         }
         return updated;
     }
+
     @GetMapping("/read")
     public Properties read() throws Exception {
         Properties prop = new Properties();
         prop.load(new FileInputStream("/var/test.properties"));
         return prop;
+
     }
 
 }
