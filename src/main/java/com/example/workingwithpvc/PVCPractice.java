06a5553ad65b2f4911a3cd9655f80b79644af833
bereket129 <abusha129@gmail.com>
2022-03-23 13:58:30 -0500

    Update PVCPractice.java

--- src/main/java/com/example/workingwithpvc/PVCPractice.java
+++ src/main/java/com/example/workingwithpvc/PVCPractice.java
@@ -9,17 +9,18 @@ import java.util.logging.Logger;
 @RestController
 public class PVCPractice {
     public final Logger LOGGER = Logger.getLogger(this.getClass().getName());
+    private String path = "/var/pvc/test.properties";
 
     @GetMapping("/write")
     public Boolean hello(@RequestParam String key, @RequestParam String value) throws IOException {
         Properties prop = new Properties();
-        FileInputStream fileInputStream = new FileInputStream("/var/test.properties");
-        prop.load(new FileInputStream("/var/test.properties"));
+        FileInputStream fileInputStream = new FileInputStream(path);
+        prop.load(new FileInputStream(path));
         fileInputStream.close();
         String oldvalue = (String) prop.setProperty(key,value);
         boolean updated = false;
         if (!value.equals(oldvalue)){
-            FileWriter fileWriter = new FileWriter("src/main/java/com/example/workingwithpvc/test.properties");
+            FileWriter fileWriter = new FileWriter(path);
             prop.store(fileWriter,"added value "+value);
             fileWriter.close();
             updated = true;
@@ -31,7 +32,7 @@ public class PVCPractice {
     @GetMapping("/read")
     public Properties read() throws Exception {
         Properties prop = new Properties();
-        FileInputStream fileInputStream = new FileInputStream("/var/test.properties");
+        FileInputStream fileInputStream = new FileInputStream(path);
         prop.load(fileInputStream);
         fileInputStream.close();
         return prop;
